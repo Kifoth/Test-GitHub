dcf4ba87419f1ad0ea05c31f17529d2d98f850d7
Przemek Stajniak <115403078+Kifoth@users.noreply.github.com>
2022-10-24 10:31:04 +0200

    Zadanie

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+
+
+
+
+            public static void main(String[] args) {
+
+
+                int[] tab = new int[10];
+
+                Scanner odczyt = new Scanner(System.in);
+                //pobieranie danych
+                for (int i=0; i<tab.length; i++) {
+                    System.out.println("podaj liczbe");
+                    tab[i] = odczyt.nextInt();
+                }
+                //wyświetlanie danych
+                for(int i=0; i<tab.length; i++)
+                    System.out.println("Liczba: "+tab[i]);
+
+
+
+
+            }
+
+        }
+
+
+
+
